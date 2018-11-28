49b4e0ee49b6a7ec7be4ba309eab9f08c1074154
Duncan <buzzbuzz.macdonald@gmail.com>
2018-11-28 11:34:18 -0800

    Initial Commit

---
+++ DuncNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DuncNotes
+//
+//  Created by Duncan MacDonald on 11/28/18.
+//  Copyright © 2018 Duncan MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
